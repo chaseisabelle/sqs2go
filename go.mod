4e6ec24013e4b743bb551a1b1c986cab1c6f458f
chase <chase.isabelle@vestiairecollective.com>
2020-12-13 12:19:43 +0100

    added nsq

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/chaseisabelle/flagz v0.0.0-20201003125634-99eb9812d59a
 	github.com/chaseisabelle/sqsc v0.0.0-20201003114639-3192a8a9cb24
 	github.com/chaseisabelle/stop v0.0.0-20201003115420-86920b703005
+	github.com/nsqio/go-nsq v1.0.8
 )
---
+++ sqs2nsq/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"github.com/chaseisabelle/sqs2_"
+	"github.com/chaseisabelle/sqs2_/config"
+	nsq "github.com/nsqio/go-nsq"
+)
+
+var producer *nsq.Producer
+var topic *string
+
+func main() {
+	to := flag.String("to", "127.0.0.1:4150", "the nsq host to forward the messages to")
+	topic = flag.String("topic", "", "the nsq topic to publish to")
+
+	sqs, err := sqs2_.New(config.Load(), handler, func(err error) {
+		println(err.Error())
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	producer, err = nsq.NewProducer(*to, nsq.NewConfig())
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = sqs.Start()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(bod string) error {
+	return producer.Publish(*topic, []byte(bod))
+}
